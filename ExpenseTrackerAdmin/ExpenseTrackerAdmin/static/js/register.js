3bb26a2629e631da6376cad151d32f5af91bd07b
Anar Bayramov <bayramovanar1703@gmail.com>
2022-11-08 18:07:12 +0400

    user verification

---
+++ ExpenseTrackerAdmin/ExpenseTrackerAdmin/static/js/register.js
@@ -0,0 +1,19 @@
+const usernameField = document.querySelector('#usernameField');
+const feedbackArea = document.querySelector('.invalid_feedback')
+usernameField.addEventListener('keyup', (e) =>{
+ const usernameValue = e.target.value;
+
+    usernameField.classList.remove("is-invalid")
+    feedbackArea.style.display='none'
+
+    if(usernameValue.length > 0){
+       fetch('/authentication/validate-username', {
+                body: JSON.stringify({username: usernameValue}),
+                method: "POST"
+        }).then(res=>res.json()).then(data=>{if(data.username_error){
+            usernameField.classList.add("is-invalid")
+           feedbackArea.style.display='block'
+           feedbackArea.innerHTML = `<p>${data.username_error}</\p>`
+       }});
+    }
+});
